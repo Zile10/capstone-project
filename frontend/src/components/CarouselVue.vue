b2a30b6298743df94546537976d3419fb268ae9a
Zile10 <johnnoad04@gmail.com>
2023-03-13 16:57:10 +0200

    Home Page Layout

---
+++ frontend/src/components/CarouselVue.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="carouselExampleCaptions" class="carousel slide">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+
+      <div v-for="slide in slides" :key="slide" class="carousel-item" :class="{active: slide == slides[0]}">
+        <img :src="slide.imgSrc" class="d-block w-100" :alt="slide.title">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>{{slide.title}}</h5>
+          <p>{{slide.description}}</p>
+        </div>
+      </div>
+
+    </div>
+    
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</template>
+<script>
+export default {
+  props: ["slides"],
+  components: {
+  }
+}
+</script>
+<style>
+  /* .carousel{
+
+    width: 500px !important;
+    height: 300px !important;
+  }
+  .carousel-item {
+    max-height: 300px;
+  } */
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,119 @@
 <template>
   <NavBar/>
   <div class="home">
-    
+    <div class="hero-banner">
+      <div class="left text-start d-flex flex-column justify-content-evenly">
+        <h1>Strategise to <br>Reach Your Life <br>Objectives</h1>
+        <p>Stratagem will provide you with the community and the resources you'll need to take control of your life and plan your way to success!</p>
+        <div class="btns d-flex justify-content-between">
+          <button type="button" class="btn btn-dark rounded-0 bg-black text-white m-auto">Register for Free</button>
+          <button type="button" class="btn btn-light border-dark rounded-0 m-auto">Login</button>
+
+        </div>
+      </div>
+      <div class="right">
+        <!-- <img src="https://i.postimg.cc/ZKfW0vgW/Robert-Greene-chessbg.webp" alt=""> -->
+      </div>
+    </div>
+
+    <div class="daily-book">
+      <div class="left"></div>
+      <div class="right d-flex flex-column justify-content-evenly">
+        <h2>Strategise to Reach Your Life Objectives</h2>
+        <div>
+          <p>Stratagem will provide you with the community and the resources you'll need to take control of your life and plan your way to success!</p>
+          <!-- <button type="button" class="btn btn-dark rounded-0 bg-black text-white m-auto">Register for Free</button> -->
+          <button type="button" class="btn btn-light border-dark rounded-0 mt-0">Learn More</button>
+
+        </div>
+      </div>
+    </div>
+
+    <!-- <div class="right">
+      <Carousel :slides="carouselSlides"/>
+    </div> -->
 
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Carousel from '@/components/CarouselVue.vue'
 
 export default {
   name: 'HomeView',
   components: {
+    Carousel
+  },
+  data() {
+    return {
+      carouselSlides: [
+        {
+          imgSrc: 'https://i.postimg.cc/52TjSdf1/robert-greene-books.webp',
+          title: "",
+          description: ""
+        }, 
+        {
+          imgSrc: 'https://i.postimg.cc/g2wHdG7C/Robert-Greene-chessbg.webp',
+          title: "",
+          description: ""
+        }, 
+        {
+          imgSrc: '',
+          title: "",
+          description: ""
+        }
+      ]
+    }
+  },
 
-  }
 }
 </script>
+
+<style>
+.hero-banner {
+  height: 70vh;
+  background-color: #ccc;
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  margin: auto;
+  width: 85%;
+}
+.left, .right{
+  width: 90%;
+  height: 90%;
+  border: 1px solid black;
+  margin: auto;
+}
+.left{
+
+}
+.hero-banner .right {
+  background-image: url(https://i.postimg.cc/g2wHdG7C/Robert-Greene-chessbg.webp);
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.daily-book {
+  height: 60vh;
+  background-color: #ccc;
+  display: grid;
+  grid-template-columns: 5fr 4fr;
+  margin: auto;
+  width: 85%;
+}
+
+.daily-book .left {
+  background-image: url(https://i.postimg.cc/bJPxQkvP/robert-greene-books.webp);
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.daily-book .right {
+  text-align: left;
+}
+.daily-book .right button {
+  width: fit-content;
+}
+
+</style>
