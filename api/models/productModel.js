3f7e042f3e8475c14651fe30e8276363db4e016a
Zile10 <johnnoad04@gmail.com>
2023-03-16 09:10:23 +0200

    added 'auther' to product queries

--- api/models/productModel.js
+++ api/models/productModel.js
@@ -3,14 +3,14 @@ const con = require('../config')
 module.exports = {
   getProducts(req, res) {
     req.user
-    con.query("SELECT prodID, prodName, prodDesc, price, stock, imgURL, category FROM products", (err, result) => {
+    con.query("SELECT prodID, prodName, author prodDesc, price, stock, imgURL, category FROM products", (err, result) => {
       if (err) throw err;
       res.status(200);
       res.send(result);
     });
   },
   getProduct(req, res) {
-    con.query("SELECT prodID, prodName, prodDesc, price, stock, imgURL, category FROM products WHERE productID = ?", [req.params.id], (err, result) => {
+    con.query("SELECT prodID, prodName, author prodDesc, price, stock, imgURL, category FROM products WHERE productID = ?", [req.params.id], (err, result) => {
       if (err) throw err;
       res.send(result[0]);
     });
