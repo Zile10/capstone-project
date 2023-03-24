5a9ba0ca1e4a902b2dd4538bcb5ef1f08355820e
John-Bruce Noad <johnnoad04@gmail.com>
2023-03-24 23:14:16 +0200

    Cart Setup Fully Fixed!!!

--- frontend/src/views/CartView.vue
+++ frontend/src/views/CartView.vue
@@ -1,8 +1,64 @@
 <template>
   <div>
-    <h1 v-for="order in cart" :key="order">
-      {{ order.prodName }}
-    </h1>
+    <h1>Cart</h1>
+    <table class="table table-dark table-striped">
+      <thead>
+        <tr>
+          <th scope="col">
+            #
+            <button class="sort-btn" onclick="sortList()" value="numUp">
+              Price:
+              <img
+                src="https://img.icons8.com/material-outlined/24/null/numerical-sorting-12.png"
+              />
+            </button>
+          </th>
+          <th scope="col">Item Name</th>
+          <th scope="col">Price</th>
+          <th scope="col">Amount In Stock</th>
+          <th scope="col">Author</th>
+          <th scope="col">Edit</th>
+          <th scope="col">Delete</th>
+        </tr>
+      </thead>
+      <tbody v-for="order in cart" :key="order.orderID" class="products-table">
+        <tr id="item-row-{{order.orderID}}">
+          <th scope="row">{{order.orderID}}</th>
+          <td>{{order.prodName}}</td>
+          <td>R{{order.price}}</td>
+          <td>{{order.stock}}</td>
+          <td>{{order.author}}</td>
+          <td>
+            <button
+              style="background-color: rgba(255, 255, 255, 0); border: none"
+              class="edit-item"
+              data-bs-toggle="modal"
+              data-bs-target="#edit-modal-{{product.prodID}}"
+              id="edit-modal-btn-{{product.prodID}}"
+            >
+              <img
+                style="height: 30px"
+                src="https://img.icons8.com/external-kiranshastry-lineal-kiranshastry/64/FFFFFF/external-edit-interface-kiranshastry-lineal-kiranshastry.png"
+              />
+            </button>
+          </td>
+          <td>
+            <button
+              style="background-color: rgba(255, 255, 255, 0); border: none"
+              class="del-item"
+              onclick="itemRemove({{product.id - 1}})"
+            >
+              <img
+                style="height: 30px"
+                src="https://img.icons8.com/material-rounded/48/FFFFFF/trash.png"
+              />
+            </button>
+          </td>
+        </tr>
+      </tbody>
+  
+      <tfoot></tfoot>
+    </table>
   </div>
 </template>
 
